69d0b41315728218621d1ebe309b2ea34a90abbb
Duck <24493647+ghostduck@users.noreply.github.com>
2017-10-29 10:08:46 +0800

    Minor change: Use list comprehension instead

--- cryptopals/tools/crypto_helper/vigenere_xor_helper.py
+++ cryptopals/tools/crypto_helper/vigenere_xor_helper.py
@@ -4,16 +4,17 @@ def VigenereXORBytes(plainBytes, keyBytes):
     # keyBytes should be shorter and will keep looping
     # This simple algorithm works even when |keyBytes| <= |plainBytes|
     keyBytesStream = cycle(keyBytes)
+    return bytearray(b ^ next(keyBytesStream) for b in plainBytes)
 
-    xorBytes = bytearray()
-    for b in plainBytes:
-        k_b = next(keyBytesStream)
-        xorBytes.append(b ^ k_b)
-    return xorBytes
+    # xorBytes = bytearray()
+    # for b in plainBytes:
+        # k_b = next(keyBytesStream)
+        # xorBytes.append(b ^ k_b)
+    # return xorBytes
 
 def VigenereXORwithRepeatedKeys(plaintext, keystr):
     plaintextBytes = bytearray(plaintext, "utf-8")
     keystrBytes = bytearray(keystr, "utf-8")
-    
+
     xorBytes = VigenereXORBytes(plaintextBytes, keystrBytes)
     return xorBytes
