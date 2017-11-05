659b041459ca80bf62051b1286d86295ab77ac89
Duck <24493647+ghostduck@users.noreply.github.com>
2017-11-05 20:49:01 +0800

    Solve Q10 by implmenting AES CBC mode
    - but I still hate those @classmethods, will try to get rid of them

--- cryptopals/tools/crypto_helper/aes.py
+++ cryptopals/tools/crypto_helper/aes.py
@@ -1,28 +1,82 @@
 # Expect users to import AES, then use AES.ECB.encrypt()/decrypt(), or AES.CBC.encrypt()/decrypt()
 
 from .aes_cipher import AES_encrypt, AES_decrypt
+from .padding_helper import PKCS7_add_padding, PKCS7_remove_padding
 
 class AES_Base(object):
     @classmethod
-    def encrypt(key_bytes=None, plain_bytes=None, **kwargs):
+    def encrypt(cls, key_bytes=None, plain_bytes=None, **kwargs):
         raise NotImplementedError("Don't use base class of AES_Base directly, implement own encrypt() instead")
 
     @classmethod
-    def decrypt(key_bytes=None, cipher_bytes=None, **kwargs):
+    def decrypt(cls, key_bytes=None, cipher_bytes=None, **kwargs):
         raise NotImplementedError("Don't use base class of AES_Base directly, implement own decrypt() instead")
 
 class ECB(AES_Base):
-    def encrypt(key_bytes=None, plain_bytes=None):
+    @classmethod
+    def encrypt(cls, key_bytes=None, plain_bytes=None):
         return AES_encrypt(key_bytes, plain_bytes)
 
-    def decrypt(key_bytes=None, cipher_bytes=None):
+    @classmethod
+    def decrypt(cls, key_bytes=None, cipher_bytes=None):
         return AES_decrypt(key_bytes, cipher_bytes)
 
 class CBC(AES_Base):
-    def encrypt(key_bytes=None, plain_bytes=None, IV=None):
-        pass
-        return AES_encrypt(key_bytes, plain_bytes)
+    @classmethod
+    def encrypt(cls, key_bytes=None, plain_bytes=None, iv=None):
+        iv = bytes(iv)
+        cls.check_iv_valid(iv)
 
-    def decrypt(key_bytes=None, cipher_bytes=None):
-        pass
-        return AES_decrypt(key_bytes, cipher_bytes)
+        plain_bytes = PKCS7_add_padding(plain_bytes)
+
+        # encrypt starts here
+        cipher_bytes = bytearray()
+
+        # Example: size 48 --> 0,1,2
+        for i in range(len(plain_bytes) // 16):
+            start = i * 16
+            end = start + 16
+
+            block_bytes = cls.block_XOR(plain_bytes[start : end], iv)
+            # Example: 0:16, 16:32, 32:48 ...
+
+            # the output is also the iv for next block
+            iv = AES_encrypt(key_bytes, block_bytes)
+
+            cipher_bytes.extend(iv)
+
+        return cipher_bytes
+
+    @classmethod
+    def decrypt(cls, key_bytes=None, cipher_bytes=None, iv=None):
+        iv = bytes(iv)
+        cls.check_iv_valid(iv)
+
+        # decrypt starts here
+        plain_bytes = bytearray()
+
+        for i in range(len(cipher_bytes) // 16):
+            start = i * 16
+            end = start + 16
+
+            block_bytes = cipher_bytes[start : end]
+
+            plain_block_bytes = AES_decrypt(key_bytes, block_bytes)
+            plain_bytes.extend(cls.block_XOR(plain_block_bytes, iv))
+
+            iv = block_bytes
+
+        plain_bytes = PKCS7_remove_padding(plain_bytes)
+        return plain_bytes
+
+    @classmethod
+    def check_iv_valid(cls, iv):
+        if len(iv) != 16:
+            raise ValueError("Block bytes not 128 bits/16 bytes")
+
+    @classmethod
+    def block_XOR(cls, ba1, ba2):
+        if len(ba1) != len(ba2):
+            raise ValueError("XOR on 2 blocks not having same length")
+
+        return bytearray([(b1^b2) for (b1,b2) in zip(ba1,ba2)])
