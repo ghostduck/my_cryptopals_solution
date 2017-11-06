ec03a2d8ff08f31bfc24a1d04321e03044ebdfa4
Duck <24493647+ghostduck@users.noreply.github.com>
2017-11-06 18:20:57 +0800

    Improve design on AES Cipher + minor change in q10

--- cryptopals/challenge_010.py
+++ cryptopals/challenge_010.py
@@ -1,15 +1,15 @@
 from tools.crypto_helper import AES
 from tools.file_helper import read_base64_encoded_text_file
 
-def check_CBC():
+def CBC_decrypt():
     file_bytes = read_base64_encoded_text_file("10.txt")
     key_bytes = bytes("YELLOW SUBMARINE", "utf-8")
     iv = bytes([0]*16)
 
     plain_bytes = AES.CBC.decrypt(cipher_bytes=file_bytes, key_bytes=key_bytes, iv=iv)
-    # same lyrics as in challenge 7 !
+    # same lyrics as in challenge 7 !!
     print(plain_bytes.decode("utf-8"))
 
 if __name__ == "__main__":
-    check_CBC()
+    CBC_decrypt()
     print("End of program")
--- cryptopals/tools/crypto_helper/aes.py
+++ cryptopals/tools/crypto_helper/aes.py
@@ -3,14 +3,31 @@
 from .aes_cipher import AES_encrypt, AES_decrypt
 from .padding_helper import PKCS7_add_padding, PKCS7_remove_padding
 
-class AES_Base(object):
+from abc import ABC, abstractmethod
+
+# helper methods -- seems module functions are good enough
+def check_iv_valid(iv):
+    if len(iv) != 16:
+        raise ValueError("Block bytes not 128 bits/16 bytes")
+
+def block_XOR(ba1, ba2):
+    if len(ba1) != len(ba2):
+        raise ValueError("XOR on 2 blocks not having same length")
+
+    return bytearray([(b1^b2) for (b1,b2) in zip(ba1,ba2)])
+
+class AES_Base(ABC):
     @classmethod
+    @abstractmethod # This decorator of abstract method should be at the deepest level
     def encrypt(cls, key_bytes=None, plain_bytes=None, **kwargs):
-        raise NotImplementedError("Don't use base class of AES_Base directly, implement own encrypt() instead")
+        pass
+        #raise NotImplementedError("Don't use base class of AES_Base directly, implement own encrypt() instead")
 
     @classmethod
+    @abstractmethod
     def decrypt(cls, key_bytes=None, cipher_bytes=None, **kwargs):
-        raise NotImplementedError("Don't use base class of AES_Base directly, implement own decrypt() instead")
+        pass
+        #raise NotImplementedError("Don't use base class of AES_Base directly, implement own decrypt() instead")
 
 class ECB(AES_Base):
     @classmethod
@@ -22,10 +39,11 @@ class ECB(AES_Base):
         return AES_decrypt(key_bytes, cipher_bytes)
 
 class CBC(AES_Base):
+    # NOTE: Decorators are not inherited!!
     @classmethod
     def encrypt(cls, key_bytes=None, plain_bytes=None, iv=None):
         iv = bytes(iv)
-        cls.check_iv_valid(iv)
+        check_iv_valid(iv)
 
         plain_bytes = PKCS7_add_padding(plain_bytes)
 
@@ -37,7 +55,7 @@ class CBC(AES_Base):
             start = i * 16
             end = start + 16
 
-            block_bytes = cls.block_XOR(plain_bytes[start : end], iv)
+            block_bytes = block_XOR(plain_bytes[start : end], iv)
             # Example: 0:16, 16:32, 32:48 ...
 
             # the output is also the iv for next block
@@ -50,7 +68,7 @@ class CBC(AES_Base):
     @classmethod
     def decrypt(cls, key_bytes=None, cipher_bytes=None, iv=None):
         iv = bytes(iv)
-        cls.check_iv_valid(iv)
+        check_iv_valid(iv)
 
         # decrypt starts here
         plain_bytes = bytearray()
@@ -62,21 +80,9 @@ class CBC(AES_Base):
             block_bytes = cipher_bytes[start : end]
 
             plain_block_bytes = AES_decrypt(key_bytes, block_bytes)
-            plain_bytes.extend(cls.block_XOR(plain_block_bytes, iv))
+            plain_bytes.extend(block_XOR(plain_block_bytes, iv))
 
             iv = block_bytes
 
         plain_bytes = PKCS7_remove_padding(plain_bytes)
         return plain_bytes
-
-    @classmethod
-    def check_iv_valid(cls, iv):
-        if len(iv) != 16:
-            raise ValueError("Block bytes not 128 bits/16 bytes")
-
-    @classmethod
-    def block_XOR(cls, ba1, ba2):
-        if len(ba1) != len(ba2):
-            raise ValueError("XOR on 2 blocks not having same length")
-
-        return bytearray([(b1^b2) for (b1,b2) in zip(ba1,ba2)])
