4e1adacf6668dcba5d8808207e3f598d61abad3b
Duck <24493647+ghostduck@users.noreply.github.com>
2017-12-03 23:00:33 +0800

    Minor update to print hex helper - accept any numbers of params like print

--- cryptopals/tools/misc_helper/print_hex_helper.py
+++ cryptopals/tools/misc_helper/print_hex_helper.py
@@ -1,4 +1,4 @@
-def print_bytes_with_description(b_s, description=""):
+def print_bytes_with_description(b_s, description="", *args):
     # print "1100ff..." from byte array(b_s)
     show_byte_message = "".join("{0:02x}".format(k) for k in b_s)
-    print(description, show_byte_message)
+    print(description, show_byte_message, *args)
